0bf4428c3b015708ada875179c8282c99781f353
saechimdaeki <anima94@kakao.com>
2022-02-08 22:10:07 +0900

    코딩으로 학습하는 리팩토링 냄새1. 이해하기 힘든 이름

---
+++ src/main/java/me/saechimdaeki/refactoring/_01_smell_mysterious_name/_03_rename_field/StudyReview.java
@@ -0,0 +1,4 @@
+package me.saechimdaeki.refactoring._01_smell_mysterious_name._03_rename_field;
+
+public record StudyReview(String reviewr, String review) {
+}
---
+++ src/test/java/me/saechimdaeki/refactoring/RefactoringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.saechimdaeki.refactoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
