1115b6e8c11cd39f5e78fefb88f81f36cd03dd4f
Junseong kim <anima94@kakao.com>
2022-02-11 08:25:43 +0900

    docs : edit readme
    코딩으로 학습하는 리팩토링 냄새 5. 전역데이터
    
    docs: update 전역데이터 Md

---
+++ src/main/java/me/saechimdaeki/refactoring/_05_global_data/_01_before/Thermostats.java
@@ -0,0 +1,13 @@
+package me.saechimdaeki.refactoring._05_global_data._01_before;
+
+public class Thermostats {
+
+    public static Integer targetTemperature = 70;
+
+    public static Boolean heating = true;
+
+    public static Boolean cooling = false;
+
+    public static Boolean fahrenheit = true;
+
+}
