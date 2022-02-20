9c9456ff1cb3b4278819216ebfb34fd2cb84f01a
saechimdaeki <anima94@kakao.com>
2022-02-20 11:54:11 +0900

    코딩으로 학습하는 리팩토링 냄새 12. 반복되는 switch 문

---
+++ src/main/java/me/saechimdaeki/refactoring/_12_repeated_swtiches/SwitchImprovements.java
@@ -0,0 +1,13 @@
+package me.saechimdaeki.refactoring._12_repeated_swtiches;
+
+public class SwitchImprovements {
+
+    public int vacationHours(String type) {
+        return switch (type) {
+            case "full-time" -> 120;
+            case "part-time" -> 80;
+            case "temporal" -> 32;
+            default -> 0;
+        };
+    }
+}
